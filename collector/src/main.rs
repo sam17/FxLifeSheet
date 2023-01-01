41d6b6591cef68819e422f450e8600117f4d333c
Charu Tak <charu120995@gmail.com>
2023-01-02 00:13:37 +0530

    Added basic test

--- collector/src/main.rs
+++ collector/src/main.rs
@@ -1,28 +1,33 @@
-use actix_web::{get, web, App, HttpServer, Responder, HttpRequest, HttpResponse};
-use serde::Deserialize;
+use actix_web::{get, web, App, HttpServer, Responder, HttpRequest};
+use serde::{Serialize, Deserialize};
 
-
-#[derive(Debug, Deserialize)]
-struct Params {
-    name: String,
-    color: String,
+#[derive(Debug, Serialize, Deserialize)]
+struct IntegrationQueryParams {
+    integration_name: String,
+    start_date: String,
+    end_date: String,
 }
 
-#[get("/path/to/page")]
-async fn handler(req: HttpRequest) -> HttpResponse {
-    let params = web::Query::<Params>::from_query(req.query_string()).unwrap();
-    HttpResponse::Ok().body(format!("{:?}", params))
+#[get("/api/v1/getData")]
+async fn get_data(req: HttpRequest) -> impl Responder {
+    // let params = web::Query::<IntegrationQueryParams>::from_query(req.query_string()).unwrap();
+    let params = IntegrationQueryParams {
+        integration_name: req.query_string().to_string(),
+        start_date: "2020-01-01".to_string(),
+        end_date: "2020-01-01".to_string(),
+    };
+    return web::Json(params);
 }
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+#[get("/")]
+async fn greet() -> impl Responder {
+    format!("Hello bobo")
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(greet).service(handler)
+        App::new().service(greet).service(get_data)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
