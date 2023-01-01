2ec71eba753e7b57880a2f3a15497b5a4443c699
Charu Tak <charu120995@gmail.com>
2023-01-01 23:10:27 +0530

    Query parameters

--- collector/src/main.rs
+++ collector/src/main.rs
@@ -1,6 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder, HttpRequest, HttpResponse};
+use serde::Deserialize;
+
+
+#[derive(Debug, Deserialize)]
+struct Params {
+    name: String,
+    color: String,
 }
 
+#[get("/path/to/page")]
+async fn handler(req: HttpRequest) -> HttpResponse {
+    let params = web::Query::<Params>::from_query(req.query_string()).unwrap();
+    HttpResponse::Ok().body(format!("{:?}", params))
+}
 
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
 
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(greet).service(handler)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
