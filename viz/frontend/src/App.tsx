ceced09aafd38cb961d65cc44448b5556218eb69
soumyadeep <soumyadeep9@gmail.com>
2023-01-29 18:37:33 +0530

    beauty started

--- viz/frontend/src/App.tsx
+++ viz/frontend/src/App.tsx
@@ -1,26 +1,53 @@
     import React from "react";
     import "./App.scss";
+    import { Row, Col } from 'antd';
     import CalendarViz from "./components/CalendarViz";
 
     interface IProps {}
-    interface IState {}
+    interface IState {
+        name: string;
+    }
 
     class App extends React.Component<IProps, IState> {
+      constructor(props: IProps) {
+          super(props);
+          this.state = {
+              name: "unnamed"
+          }
+      }
 
-      baseUrl: string = "/api/data/";
+      development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      baseUrl : string = this.development?"https://metrics.soumyadeep.in/api/data/": "/api/data/";
+
+      getMetadata = () => {
+        return fetch(this.baseUrl + "metadata").then(response => response.json());
+      }
+
+      componentDidMount() {
+          this.getMetadata().then(data => {
+              console.log(data);
+              this.setState({name: data.name});
+          }).catch(error => {
+              console.log(error);
+              this.setState({name: "Error"});
+          })
+      }
 
       render() {
         return (
             <div className="App">
-              <CalendarViz name="happyLevels"  url={this.baseUrl}/>
-              <CalendarViz name="anxietyLevels"  url={this.baseUrl}/>
-              <CalendarViz name="energyLevels"  url={this.baseUrl}/>
-              <CalendarViz name="learned"  url={this.baseUrl}/>
-              <CalendarViz name="socializing"  url={this.baseUrl}/>
-              <CalendarViz name="parents"  url={this.baseUrl}/>
-              <CalendarViz name="lays"  url={this.baseUrl}/>
-              <CalendarViz name="gym"  url={this.baseUrl}/>
-              <CalendarViz name="numberOfMeals"  url={this.baseUrl}/>
+                <h1><center>How is {this.state.name}?</center></h1>
+                <Col>
+                    <CalendarViz name="happyLevels"  url={this.baseUrl}/>
+                    <CalendarViz name="anxietyLevels"  url={this.baseUrl}/>
+                </Col>
+                <CalendarViz name="energyLevels"  url={this.baseUrl}/>
+                <CalendarViz name="learned"  url={this.baseUrl}/>
+                <CalendarViz name="socializing"  url={this.baseUrl}/>
+                <CalendarViz name="parents"  url={this.baseUrl}/>
+                <CalendarViz name="lays"  url={this.baseUrl}/>
+                <CalendarViz name="gym"  url={this.baseUrl}/>
+                <CalendarViz name="numberOfMeals"  url={this.baseUrl}/>
             </div>
         );
       }
