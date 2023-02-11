42a7c1f60f77a1998f073d6c9155f5998846a77b
Charu Tak <charu120995@gmail.com>
2023-02-11 21:20:55 +0530

    Added categories

--- viz/backend/src/model/mod.rs
+++ viz/backend/src/model/mod.rs
@@ -2,6 +2,7 @@ mod db;
 mod raw_data_dao;
 mod viz_metadata_dao;
 mod viz_questions_dao;
+mod viz_categories_dao;
 
 // re-export
 pub use db::init_db;
@@ -9,6 +10,7 @@ pub use db::Db;
 pub use raw_data_dao::{RawData, RawDataObj};
 pub use viz_metadata_dao::{VizMetadata, VizMetadataObj};
 pub use viz_questions_dao::{VizQuestions, VizQuestionsObj};
+pub use viz_categories_dao::{VizCategories, VizCategoriesObj};
 
 // region:    Error
 #[derive(thiserror::Error, Debug)]
