7fb3b1721d3f6bff44c29ea20eb153fdc0615f6a
soumyadeep <soumyadeep9@gmail.com>
2023-02-26 21:56:48 +0530

    Debug lines added and remote working with JSON parse

--- telegram_bot/src/classes/config.js
+++ telegram_bot/src/classes/config.js
@@ -2,10 +2,11 @@
 exports.__esModule = true;
 var needle = require("needle");
 var url = process.env.LIFESHEET_JSON_URL;
+console.log("Lifesheet URL: ", url);
 if (url) {
     console.log("Loading remote JSON config...");
     needle.get(url, function (error, response, body) {
-        var userConfig = body;
+        var userConfig = JSON.parse(body);
         console.log("Successfully loaded remote user config");
         module.exports.userConfig = userConfig;
     });
--- telegram_bot/src/classes/telegram.ts
+++ telegram_bot/src/classes/telegram.ts
@@ -1,6 +1,7 @@
 // Telegram setup
 const Telegraf = require("telegraf");
 
+console.log("BOT TOKEN: ", process.env.TELEGRAM_BOT_TOKEN);
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 module.exports.bot = bot;
