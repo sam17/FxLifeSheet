bc0963d8e147c58374cd0323f06eed8c83e08a67
soumyadeep <soumyadeep9@gmail.com>
2023-05-07 00:45:05 +0530

    Working options

--- backend/src/bin/web/daos/viz_questions_dao.rs
+++ backend/src/bin/web/daos/viz_questions_dao.rs
@@ -1,5 +1,7 @@
 use std::mem::size_of;
+use sqlx::postgres::PgRow;
 use models::models::questions::viz_questions::Question;
+use models::models::questions::question_options::QuestionOption;
 use crate::daos::viz_categories_dao::VizCategories;
 use crate::utils::db::Db;
 use crate::utils::error::ModelError;
@@ -19,10 +21,11 @@ impl VizQuestions {
         "is_positive",
         "display_name",
         "category",
-        "command"
+        "command",
+        "cadence",
+        "graph_type"
     ];
 }
-
 
 impl VizQuestions {
     pub async fn get_questions_with_query(
@@ -46,21 +49,41 @@ impl VizQuestions {
             sb = sb.and_where_eq("command", cmd);
         }
 
-        let questions_list = sb.fetch_all(db).await?;
+        let mut questions_list: Vec<Question> = sb.fetch_all(db).await?;
+
+        // Fetch question options for each question and update the question struct
+        for question in questions_list.iter_mut() {
+            let question_options:Vec<QuestionOption> =
+                VizQuestionOptions::get_options_for_question
+                    (db, &question.key).await?;
+            question.set_question_options(question_options);
+        }
+
         Ok(questions_list)
     }
 
-    pub async fn get_questions_for_command(
-        db: &Db,
-        command: String,
-    ) -> Result<Vec<Question>, ModelError> {
-        let mut sb = sqlb::select().table(Self::TABLE).columns(Self::COLUMNS);
-        sb = sb.and_where_eq("command", command);
-
-        let questions_list = sb.fetch_all(db).await?;
-        // print size of questions_list
-        println!("questions_list size: {}", size_of::<Vec<Question>>());
-        Ok(questions_list)
-    }
 }
 
+pub struct VizQuestionOptions;
+
+impl VizQuestionOptions{
+    const TABLE: &'static str = "options";
+    const COLUMNS: &'static [&'static str] = &[
+        "id",
+        "question_key",
+        "name",
+    ];
+}
+
+impl VizQuestionOptions{
+    pub async fn get_options_for_question(
+        db: &Db,
+        question_key: &str,
+    ) -> Result<Vec<QuestionOption>, ModelError> {
+        let mut sb = sqlb::select().table(Self::TABLE).columns(Self::COLUMNS);
+        sb = sb.and_where_eq("question_key", question_key);
+
+        let options_list: Vec<QuestionOption> = sb.fetch_all(db).await?;
+        Ok(options_list)
+    }
+}
